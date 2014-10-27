a74d1713a4269dcee8370a106426e3f56d83bc33
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2014-10-27 13:53:27 -0700

    foo

--- src/rougelike.lisp
+++ src/rougelike.lisp
@@ -15,6 +15,7 @@
 (defparameter *screen-height* 30)
 (defparameter *interval* .01)
 (defparameter *running* nil)
+(defparameter *screens* (make-hash-table))
 
 (defclass screen ()
   ((window 
@@ -32,6 +33,9 @@
 
 (defgeneric draw-screen (screen)
   (:documentation "render output"))
+
+(defgeneric next-screen (screen)
+  (:documentation "returns the next screen to be run"))
 
 
 (defmethod draw-screen ((screen win-screen))
@@ -56,27 +60,41 @@
           ((#\t) (write-at-point "win screen input" 3 3)))))
 
 
+(defmethod next-screen ((screen lose-screen))
+  nil)
+
+(defmethod next-screen ((screen win-screen))
+  (gethash 'lose *screens*))
+
 
 (defun run-screen (screen)
-  (clear-screen)
-  (setf *running* t)
-    (loop :while *running*
-          :do
-          (refresh-window *standard-window*)
-          (sleep *interval*  )
-          (get-input screen )
-          (draw-screen screen )))
+  (if screen
+    (progn (clear-screen)
+           (setf *running* t)
+           (loop :while *running*
+                 :do
+                 (refresh-window *standard-window*)
+                 (sleep *interval*  )
+                 (get-input screen )
+                 (draw-screen screen ))
+           (run-screen (next-screen screen)))
+    (sb-sys:os-exit 0)))
 
-
-(defparameter *lose* nil)
-(defparameter *win* nil)
 
 (defun main ()
   (with-init
-    (setf *win* (make-instance 'win-screen))
-    (setf *lose* (make-instance 'lose-screen))
-    (run-screen *win*)
-    (run-screen *lose*)
+    (setf (gethash 'win *screens*)  (make-instance 'win-screen))
+    (setf (gethash 'lose *screens*)  (make-instance 'lose-screen))
+    (run-screen (gethash 'win *screens*))
+    ;(run-screen *lose*)
     ))
 
 (main)
+
+;(setq myh (make-hash-table))
+;
+;(gethash 'foo myh)
+;(setf (gethash 'foo myh) "my foo")
+;(setf (gethash 'f2oo myh) "my foo2")
+;
+;(maphash #'(lambda (key val) (format t "~A ~A ~%" key val )) myh)
