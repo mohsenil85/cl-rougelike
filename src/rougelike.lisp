4f2bd9549563abe988cb0f04ba6a3a2859c573e0
Logan Mohseni <logan.mohseni@singlemindconsulting.com>
2014-10-28 14:38:13 -0700

    use structs not objects

--- src/rougelike.lisp
+++ src/rougelike.lisp
@@ -17,98 +17,32 @@
 (defparameter *running* nil)
 (defparameter *screens* (make-hash-table))
 
-(defclass screen ()
-  ((window 
-     :initarg :window
-     :initform *standard-window*)) )
+(defstruct screen input output next)
 
-(defclass lose-screen (screen)
-  (window))
+(defmacro defscreen (screen-name &key input output next)
+  `(setf (gethash ',screen-name *screens*) 
+         (make-screen 
+           :input 
+           '(let  ((c (get-char *standard-window* :ignore-error t)))
+              (case c
+                ,@input))
 
-(defclass win-screen (screen)
-  (window))
+           :output ',@output
 
-(defgeneric get-input (screen)
-  (:documentation "collect input"))
+           :next '(gethash ,next *screens*) )))
 
-(defgeneric draw-screen (screen)
-  (:documentation "render output"))
+(defscreen win
+           :input ( ((nil) nil)
+                    ((#\q) (setf *running* nil))
+                    ((#\t) (write-at-point "win screen input" 3 3)))
+           :output ((write-at-point "i am the win screen" 0 0))
+           :next 'lose)
 
-(defgeneric next-screen (screen)
-  (:documentation "returns the next screen to be run"))
-
-
-(defmethod draw-screen ((screen win-screen))
-  (write-at-point "i am the win screen" 0 0))
-
-(defmethod draw-screen ((screen lose-screen))
-  (write-at-point "i am the lose screen" 0 0))
-
-
-(defmethod get-input ((screen lose-screen))
-      (let  ((c (get-char *standard-window* :ignore-error t)))
-        (case c
-          ((nil) nil)
-          ((#\q) (setf *running* nil))
-          ((#\t) (write-at-point "lose screen input" 3 3)))))
-
-(defmethod get-input ((screen win-screen))
-      (let  ((c (get-char *standard-window* :ignore-error t)))
-        (case c
-          ((nil) nil)
-          ((#\q) (setf *running* nil))
-          ((#\t) (write-at-point "win screen input" 3 3)))))
-
-
-(defun make-start-screen ()
-  (progn
-    (defclass start-screen (screen) 
-      (window))
-
-    (defmethod draw-screen ((screen start-screen))
-      (write-at-point "i am the start screen" 0 0))
-
-    (defmethod get-input ((screen start-screen))
-      (let  ((c (get-char *standard-window* :ignore-error t)))
-        (case c
-          ((nil) nil)
-          ((#\q) (setf *running* nil))
-          ((#\t) (write-at-point "start screen input" 3 3)))) )
-
-    (defmethod next-screen ((screen start-screen))
-      (gethash 'win *screens*))
-
-    (setf (gethash 'start *screens*)  (make-instance 'start-screen))))
-
-(defmacro defscreen (scrn )
-  `(progn
-     (defclass ,scrn (screen)
-       (window))
-
-     (defmethod draw-screen ((screen ,scrn))
-        (write-at-point "i am the start screen" 0 0))
-
-     (defmethod get-input ((screen ,scrn))
-        (let  ((c (get-char *standard-window* :ignore-error t)))
-          (case c
-            ((nil) nil)
-            ((#\q) (setf *running* nil))
-            ((#\t) (write-at-point "start screen input" 3 3))))  )
-
-     (defmethod next-screen ((screen ,scrn))
-        (gethash 'win *screens*))
-
-     (setf (gethash ',scrn *screens*)  (make-instance ',scrn))) )
-
-(macroexpand-1 '(defscreen start-screen))
-
-
-(defmethod next-screen ((screen lose-screen))
-  nil)
-
-(defmethod next-screen ((screen win-screen))
-  (gethash 'lose *screens*))
-
+(defscreen lose
+           :input ( ((nil) nil)
+                    ((#\q) (setf *running* nil))
+                    ((#\t) (write-at-point "lose screen input" 3 3)))
+           :output ((write-at-point "i am the lose screen" 0 0)))
 
 (defun run-screen (screen)
   (if screen
@@ -118,27 +52,13 @@
                  :do
                  (refresh-window *standard-window*)
                  (sleep *interval*  )
-                 (get-input screen )
-                 (draw-screen screen ))
-           (run-screen (next-screen screen)))
+                 (eval (screen-input screen ))
+                 (eval (screen-output screen )))
+           (run-screen (eval (screen-next screen))))
     (sb-sys:os-exit 0)))
-
 
 (defun main ()
   (with-init
-    (make-start-screen)
-;    (defscreen start-screen)
-    (setf (gethash 'win *screens*)  (make-instance 'win-screen))
-    (setf (gethash 'lose *screens*)  (make-instance 'lose-screen))
-    (run-screen (gethash 'start *screens*))
-    ))
+    (run-screen (gethash 'win *screens*))))
 
 (main)
-
-;(setq myh (make-hash-table))
-;
-;(gethash 'foo myh)
-;(setf (gethash 'foo myh) "my foo")
-;(setf (gethash 'f2oo myh) "my foo2")
-;
-;(maphash #'(lambda (key val) (format t "~A ~A ~%" key val )) myh)
